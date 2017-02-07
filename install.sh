85ae09e6e1a20a892630c88514ab5dbca6d370c5
shruthi2506 <shruthirao2596@gmail.com>
2017-02-07 11:19:47 +0000

    Updated CLI versions

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
-#!/bin/bash -e
-
+#!/bin/bash -e 
+export TF_INSTALL_LOCATION=/opt
+export TF_VERSION=0.8.5
+export PK_INSTALL_LOCATION=/opt
+export PK_VERSION=0.12.2
+export PK_FILENAME=packer_"$PK_VERSION"_linux_amd64.zip
 locale-gen en_US en_US.UTF-8 && dpkg-reconfigure locales
 
 cd /u16
@@ -15,7 +19,8 @@ apt-get update
 
 echo "================= Installing basic packages ==================="
 apt-get install -y \
-  sudo  \
+  sudo\
+  vim\
   build-essential \
   curl \
   gcc \
@@ -37,37 +42,71 @@ apt-get install -y \
 
 pip install virtualenv
 
+echo "================= Adding Terraform 0.8.5 ==================="
+pushd $TF_INSTALL_LOCATION
+echo "Fetching terraform"
+echo "-----------------------------------"
+
+rm -rf $TF_INSTALL_LOCATION/terraform
+mkdir -p $TF_INSTALL_LOCATION/terraform
+
+wget -q https://releases.hashicorp.com/terraform/$TF_VERSION/terraform_"$TF_VERSION"_linux_amd64.zip
+apt-get install unzip
+unzip -o terraform_"$TF_VERSION"_linux_amd64.zip -d $TF_INSTALL_LOCATION/terraform
+echo "export PATH=$PATH:$TF_INSTALL_LOCATION/terraform" >> ~/.bashrc
+echo "Installed terraform successfully"
+echo "-----------------------------------"
+popd  
+
+echo "================= Adding Packer 0.12.2 ==================="
+pushd $PK_INSTALL_LOCATION
+echo "Fetching packer"
+echo "-----------------------------------"
+
+rm -rf $PK_INSTALL_LOCATION/packer
+mkdir -p $PK_INSTALL_LOCATION/packer
+
+wget -q https://releases.hashicorp.com/packer/$PK_VERSION/"$PK_FILENAME"
+unzip -o $PK_FILENAME -d $PK_INSTALL_LOCATION/packer
+echo "export PATH=$PATH:$PK_INSTALL_LOCATION/packer" >> ~/.bashrc
+echo "Installed packer successfully"
+echo "-----------------------------------"
+popd
+
+echo "================= Adding JQ 1.5.1 ==================="
+apt-get install jq
+
+
 echo "================= Installing Git ==================="
 add-apt-repository ppa:git-core/ppa -y
 apt-get update
 apt-get install -y git
 
 echo "================= Installing Node 7.x ==================="
-. node/install.sh
+. /u16/node/install.sh
 
 echo "================= Installing Java 1.8.0 ==================="
-. java/install.sh
+. /u16/java/install.sh
 
 echo "================= Installing Ruby 2.3.3  ==================="
-. ruby/install.sh
+. /u16/ruby/install.sh
 
 echo "================= Adding gcloud ============"
 CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
 
-echo "================= Adding awscli 1.11.38 ============"
-sudo pip install 'awscli==1.11.38'
+echo "================= Adding awscli 1.11.44 ============"
+sudo pip install 'awscli==1.11.44'
 
 echo "================= Adding awsebcli 3.9.0 ============"
 sudo pip install 'awsebcli==3.9.0'
 
-echo "================= Adding jfrog-cli 1.6.0 ==================="
-wget -v https://api.bintray.com/content/jfrog/jfrog-cli-go/1.6.0/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64 -O jfrog
+echo "================= Adding jfrog-cli 1.7.0 ==================="
+wget -v https://api.bintray.com/content/jfrog/jfrog-cli-go/1.7.0/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64 -O jfrog
 sudo chmod +x jfrog
 mv jfrog /usr/bin/jfrog
-
 echo "================= Cleaning package lists ==================="
 apt-get clean
 apt-get autoclean
