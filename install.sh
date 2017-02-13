715becce0f3a6b03ae56d82b6b6f858e2356a2ec
Avi Cavale <avi@shippable.com>
2017-02-13 13:43:04 +0530

    updating base packages to conform to microbase

--- install.sh
+++ install.sh
@@ -1,86 +1,57 @@
 #!/bin/bash -e 
-export TF_INSTALL_LOCATION=/opt
-export TF_VERSION=0.8.5
-export PK_INSTALL_LOCATION=/opt
-export PK_VERSION=0.12.2
-export PK_FILENAME=packer_"$PK_VERSION"_linux_amd64.zip
-locale-gen en_US en_US.UTF-8 && dpkg-reconfigure locales
 
+dpkg-divert --local --rename --add /sbin/initctl
+locale-gen en_US en_US.UTF-8
+dpkg-reconfigure locales
+
+echo "HOME=$HOME"
 cd /u16
+
+echo "================= Updating package lists ==================="
+apt-get update
 
 echo "================= Adding some global settings ==================="
 mv gbl_env.sh /etc/profile.d/
 mkdir -p $HOME/.ssh/
 mv config $HOME/.ssh/
 mv 90forceyes /etc/apt/apt.conf.d/
-
-echo "================= Updating package lists ==================="
-apt-get update
+touch $HOME/.ssh/known_hosts
 
 echo "================= Installing basic packages ==================="
 apt-get install -y \
-  sudo\
-  vim\
-  build-essential \
-  curl \
-  gcc \
-  make \
-  netcat \
-  openssl \
-  software-properties-common \
-  wget \
-  nano \
-  unzip \
-  libxslt-dev \
-  libxml2-dev
+    sudo  \
+    build-essential \
+    curl \
+    gcc \
+    make \
+    openssl \
+    software-properties-common \
+    wget \
+    nano \
+    unzip \
+    openssh-client \
+    libxslt-dev \
+    libxml2-dev \
+    htop \
+    texinfo \
+    g++-4.9 \
 
 echo "================= Installing Python packages ==================="
 apt-get install -y \
   python-pip \
   python-software-properties \
+  software-properties-common \
   python-dev
 
 pip install virtualenv
-
-echo "================= Adding Terraform 0.8.5 ==================="
-pushd $TF_INSTALL_LOCATION
-echo "Fetching terraform"
-echo "-----------------------------------"
-
-rm -rf $TF_INSTALL_LOCATION/terraform
-mkdir -p $TF_INSTALL_LOCATION/terraform
-
-wget -q https://releases.hashicorp.com/terraform/$TF_VERSION/terraform_"$TF_VERSION"_linux_amd64.zip
-apt-get install unzip
-unzip -o terraform_"$TF_VERSION"_linux_amd64.zip -d $TF_INSTALL_LOCATION/terraform
-echo "export PATH=$PATH:$TF_INSTALL_LOCATION/terraform" >> ~/.bashrc
-echo "Installed terraform successfully"
-echo "-----------------------------------"
-popd  
-
-echo "================= Adding Packer 0.12.2 ==================="
-pushd $PK_INSTALL_LOCATION
-echo "Fetching packer"
-echo "-----------------------------------"
-
-rm -rf $PK_INSTALL_LOCATION/packer
-mkdir -p $PK_INSTALL_LOCATION/packer
-
-wget -q https://releases.hashicorp.com/packer/$PK_VERSION/"$PK_FILENAME"
-unzip -o $PK_FILENAME -d $PK_INSTALL_LOCATION/packer
-echo "export PATH=$PATH:$PK_INSTALL_LOCATION/packer" >> ~/.bashrc
-echo "Installed packer successfully"
-echo "-----------------------------------"
-popd
-
-echo "================= Adding JQ 1.5.1 ==================="
-apt-get install jq
-
 
 echo "================= Installing Git ==================="
 add-apt-repository ppa:git-core/ppa -y
 apt-get update
 apt-get install -y git
+
+echo "================= Adding JQ 1.5.1 ==================="
+apt-get install jq
 
 echo "================= Installing Node 7.x ==================="
 . /u16/node/install.sh
@@ -97,6 +68,11 @@ echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee /etc/
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 sudo apt-get update && sudo apt-get install google-cloud-sdk
 
+echo "================= Adding kubectl 1.5.1 ==================="
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.5.1/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
 echo "================= Adding awscli 1.11.44 ============"
 sudo pip install 'awscli==1.11.44'
 
@@ -107,6 +83,38 @@ echo "================= Adding jfrog-cli 1.7.0 ==================="
 wget -v https://api.bintray.com/content/jfrog/jfrog-cli-go/1.7.0/jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64 -O jfrog
 sudo chmod +x jfrog
 mv jfrog /usr/bin/jfrog
+
+echo "================ Adding terraform-0.8.6===================="
+export TF_INSALL_LOCATION=/opt
+export TF_VERSION=0.8.6
+
+echo "Fetching terraform"
+echo "-----------------------------------"
+rm -rf $TF_INSALL_LOCATION/terraform
+mkdir -p $TF_INSALL_LOCATION/terraform
+wget -q https://releases.hashicorp.com/terraform/$TF_VERSION/terraform_"$TF_VERSION"_linux_386.zip
+unzip -o terraform_"$TF_VERSION"_linux_386.zip -d $TF_INSALL_LOCATION/terraform
+echo "export PATH=$PATH:$TF_INSALL_LOCATION/terraform" >> ~/.bashrc
+export PATH=$PATH:$TF_INSALL_LOCATION/terraform
+echo "Added terraform successfully"
+echo "-----------------------------------"
+
+echo "================ Adding packer 0.12.2 ===================="
+export PK_INSALL_LOCATION=/opt
+export PK_VERSION=0.12.2
+export PK_FILENAME=packer_"$PK_VERSION"_linux_amd64.zip
+
+echo "Fetching packer"
+echo "-----------------------------------"
+rm -rf $PK_INSALL_LOCATION/packer
+mkdir -p $PK_INSALL_LOCATION/packer
+wget -q https://releases.hashicorp.com/packer/$PK_VERSION/"$PK_FILENAME"
+unzip -o $PK_FILENAME -d $PK_INSALL_LOCATION/packer
+echo "export PATH=$PATH:$PK_INSALL_LOCATION/packer" >> ~/.bashrc
+export PATH=$PATH:$PK_INSALL_LOCATION/packer
+echo "Added packer successfully"
+echo "-----------------------------------"
+
 echo "================= Cleaning package lists ==================="
 apt-get clean
 apt-get autoclean
